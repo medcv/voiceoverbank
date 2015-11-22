351e39d79459af0db68150785f8a0324608904f7
medcv <y_shirvany@yahoo.com>
2015-11-22 16:37:53 -0500

    Revert "Updated gitignore"

--- app.js
+++ app.js
@@ -1,74 +1,65 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-var Customer   = require('./models/customer'); 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var accounts = require('./routes/accounts');
+var customers = require('./routes/customers');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('localhost:27017/voiceoverbank');
+app.use('/', routes);
+app.use('/users', users);
+app.use('/accounts', accounts);
+app.use('/customers', customers);
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+// error handlers
 
-// more routes for our API will happen here
-
-router.route('/customers')
-
-  .post(function(req, response) {
-    var customer = new Customer();
-    customer.name = req.body.name;
-
-    customer.save(function(err) {
-      if(err) {
-        response.send(err);
-      } else {
-        response.json({message: 'Customer created'});
-      }
-    })
-  })
-
-  .get(function(req, resp) {
-    Customer.find(function(err, customers) {
-      if(err) {
-        resp.send(err);
-      } else {
-        resp.json(customers);
-      }
-    })
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
+}
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+module.exports = app;
